1f7c6dc5e924ecaabd366d97d34771b8604b99ce
Jianqoq <ljj1849532909@gmail.com>
2024-06-23 20:06:42 -0400

    Refactor code to remove unused files and modules

--- tensor-compiler/src/lib.rs
+++ tensor-compiler/src/lib.rs
@@ -1,28 +1,30 @@
-use r#type::{HalideirTypeCode, Type};
+use halide::r#type::{ HalideirTypeCode, Type };
 
-pub mod expr;
-pub mod for_stmt;
-pub mod let_stmt;
-pub mod substitute {
-    pub mod subsititue_var;
-    pub mod subsititue_expr;
-    pub mod subsititue_for_range;
-    pub mod substitue_load_partial_idx;
+pub mod halide {
+    pub mod expr;
+    pub mod for_stmt;
+    pub mod let_stmt;
+    pub mod substitute {
+        pub mod subsititue_var;
+        pub mod subsititue_expr;
+        pub mod subsititue_for_range;
+        pub mod substitue_load_partial_idx;
+    }
+    pub mod utils {
+        pub mod for_utils;
+    }
+    pub mod if_stmt;
+    pub mod inplace_store_stmt;
+    pub mod r#type;
+    pub mod ir_cmp;
+    pub mod printer;
+    pub mod seq_stmt;
+    pub mod stmt;
+    pub mod store_stmt;
+    pub mod tests;
+    pub mod traits;
+    pub mod exprs;
+    pub mod variable;
 }
-pub mod utils {
-    pub mod for_utils;
-}
-pub mod if_stmt;
-pub mod inplace_store_stmt;
-pub mod r#type;
-pub mod ir_cmp;
-pub mod printer;
-pub mod seq_stmt;
-pub mod stmt;
-pub mod store_stmt;
-pub mod tests;
-pub mod traits;
-pub mod exprs;
-pub mod variable;
 
 static I64_TYPE: Type = Type::new(HalideirTypeCode::Int, 64, 1);
