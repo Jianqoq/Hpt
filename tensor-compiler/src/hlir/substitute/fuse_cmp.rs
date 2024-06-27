53ea1564dbcb13ba565b75f02d02e2e33f128c0c
Jianqoq <ljj1849532909@gmail.com>
2024-06-27 17:49:30 -0400

    Refactor code to add support for unary operations in the registry

--- tensor-compiler/src/hlir/substitute/fuse_cmp.rs
+++ tensor-compiler/src/hlir/substitute/fuse_cmp.rs
@@ -1,7 +1,24 @@
+#![allow(dead_code)]
 use hashbrown::HashMap;
 
-use crate::{ halide::variable::Variable, hlir::exprs::ComputeNode };
+use crate::{
+    halide::variable::Variable,
+    hlir::{ expr::Expr, exprs::ComputeNode, traits::{ HlirMutateVisitor, MutatorGetSet } },
+};
 
 pub struct FuseComputeNode {
     map: HashMap<Variable, ComputeNode>,
+    expr: Expr,
 }
+
+impl MutatorGetSet for FuseComputeNode {
+    fn set_expr<T: Into<Expr>>(&mut self, expr: T) {
+        self.expr = expr.into();
+    }
+
+    fn expr(&self) -> &Expr {
+        &self.expr
+    }
+}
+
+impl HlirMutateVisitor for FuseComputeNode {}
