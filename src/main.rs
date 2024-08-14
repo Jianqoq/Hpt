830c430c771b14b33a27dcd4774df2e4bb9ae6cb
Jianqoq <ljj1849532909@gmail.com>
2024-08-14 01:00:55 -0400

    Refactor conv2d function for improved performance and readability

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,12 @@ use tensor_dyn::*;
 fn main() -> anyhow::Result<()> {
     set_global_display_lr_elements(10);
     let kernel = _Tensor::<f32>::arange(0, 480)?.reshape([3, 10, 4, 4])?.permute([2, 3, 0, 1])?.contiguous()?;
-    let a = _Tensor::<f32>::arange(0, 750000)?.reshape([3, 500, 500])?; // shape [2, 2, 5, 5]
+    let a = _Tensor::<f32>::arange(0, 750000)?.reshape([500, 500, 3])?; // shape [2, 2, 5, 5]
     let now = std::time::Instant::now();
     let res = conv2d(&a, &kernel, [1, 1])?;
 
     println!("{:?}", now.elapsed());
 
-    // println!("{:?}", res);
 
     let kernel = _Tensor::<f32>::arange(0, 480)?.reshape([10, 3, 4, 4])?;
     let a = _Tensor::<f32>::arange(0, 750000)?.reshape([3, 500, 500])?; // shape [2, 2, 5, 5]
@@ -21,6 +20,5 @@ fn main() -> anyhow::Result<()> {
 
     println!("{:?}", now.elapsed());
 
-    // println!("{:?}", res);
     Ok(())
 }
--- tensor-dyn/src/ops/cpu/convolutions/conv2d.rs
+++ tensor-dyn/src/ops/cpu/convolutions/conv2d.rs
@@ -4,6 +4,7 @@ use tensor_traits::TensorCreator;
 use tensor_types::type_promote::NormalOut;
 use crate::tensor_base::_Tensor;
 
+#[cfg(target_feature = "fma")]
 pub fn conv2d<T>(
     img: &_Tensor<T>,
     kernels: &_Tensor<T>,
@@ -12,9 +13,9 @@ pub fn conv2d<T>(
     where T: CommonBounds + std::ops::Mul<Output = T> + std::ops::AddAssign<T> + MulAdd<Output = T>
 {
     let img_shape = img.shape();
-    let img_channels = img_shape[0];
+    let img_channels = img_shape[2];
     let img_width = img_shape[1];
-    let img_height = img_shape[2];
+    let img_height = img_shape[0];
     let kernel_shape = kernels.shape();
     let in_channels = kernel_shape[2];
     let out_channels = kernel_shape[3];
@@ -30,23 +31,25 @@ pub fn conv2d<T>(
     let out_height =
         <i64 as NormalOut<i64>>::_floor((img_height - kernel_height) / step_height) + 1;
     let out_width = <i64 as NormalOut<i64>>::_floor((img_width - kernel_width) / step_width) + 1;
-    let output = _Tensor::<T>::zeros([out_channels, out_width, out_height])?;
+    let output = _Tensor::<T>::zeros([out_width, out_height, out_channels])?;
     let mut out = output.ptr();
     let inp = img.ptr();
     let kernel = kernels.ptr();
-    let os0 = output.strides()[0];
+    let os0 = output.strides()[2];
     let os1 = output.strides()[1];
-    let os2 = output.strides()[2];
+    let os2 = output.strides()[0];
 
-    let is0 = img.strides()[0];
-    let is1 = img.strides()[1];
-    let is2 = img.strides()[2];
+    let is0 = img.strides()[1];
+    let is1 = img.strides()[2];
+    let is2 = img.strides()[0];
 
     let ks0 = kernels.strides()[2];
     let ks1 = kernels.strides()[3];
     let ks2 = kernels.strides()[1];
     let ks3 = kernels.strides()[0];
-
+    println!("kernel strides: {:?}", [ks0, ks1, ks2, ks3]);
+    println!("output strides: {:?}", [os0, os1, os2]);
+    println!("input strides: {:?}", [is0, is1, is2]);
     for l in 0..out_height {
         for n in 0..kernel_height {
             for m in 0..kernel_width {
@@ -55,10 +58,7 @@ pub fn conv2d<T>(
                         for j in 0..out_channels {
                             let k_val = kernel[i * ks0 + j * ks1 + m * ks2 + n * ks3];
                             let i_val = inp[i * is0 + (k * step_width + m) * is1 + (l * step_height + n) * is2]; // prettier-ignore
-                            out[j * os0 + k * os1 + l * os2] = i_val.mul_add(
-                                k_val,
-                                out[j * os0 + k * os1 + l * os2]
-                            );
+                            out[j * os0 + k * os1 + l * os2] += i_val * k_val;
                         }
                     }
                 }
