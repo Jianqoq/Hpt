9a6ac06146ee87561f8a3525db8b5a00793a6188
Jianqoq <ljj1849532909@gmail.com>
2024-08-15 17:40:35 -0400

    Refactor conv2d function for improved performance and readability

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,19 @@ use tensor_dyn::*;
 fn main() -> anyhow::Result<()> {
     set_global_display_lr_elements(6);
     set_num_threads(10);
+    // let kernel = _Tensor::<f32>
+    //     ::arange(0, 10240)?
+    //     .reshape([8, 80, 4, 4])?
+    //     .permute([2, 3, 0, 1])?
+    //     .contiguous()?;
+    // let a = _Tensor::<f32>
+    //     ::arange(0, 8 * 256 * 128)?
+    //     .reshape([8, 256, 128])?
+    //     .permute([1, 2, 0])?
+    //     .contiguous()?;
+    // let now = std::time::Instant::now();
+    // let res1 = conv2d_block(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
+    // println!("{:?}", now.elapsed());
     let kernel = _Tensor::<f32>
         ::arange(0, 10240)?
         .reshape([8, 80, 4, 4])?
@@ -24,21 +37,10 @@ fn main() -> anyhow::Result<()> {
         .permute([1, 2, 0])?
         .contiguous()?;
     let now = std::time::Instant::now();
-    let res1 = conv2d_block(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
-    println!("{:?}", now.elapsed());
-    let kernel = _Tensor::<f32>
-        ::arange(0, 10240)?
-        .reshape([8, 80, 4, 4])?
-        .permute([2, 3, 0, 1])?
-        .contiguous()?;
-    let a = _Tensor::<f32>
-        ::arange(0, 8 * 256 * 128)?
-        .reshape([8, 256, 128])?
-        .permute([1, 2, 0])?
-        .contiguous()?;
-    let now = std::time::Instant::now();
-    let res = conv2d_block_simd_parallel(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
-    println!("{:?}", now.elapsed());
+    for _ in 0..1000000000 {
+        let res = conv2d_block_simd_parallel(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
+    }
+    println!("{:?}", now.elapsed() / 100);
     // let kernel = _Tensor::<f32>
     //     ::arange(0, 2048)?
     //     .reshape([8, 16, 4, 4])?
@@ -48,6 +50,5 @@ fn main() -> anyhow::Result<()> {
     // let now = std::time::Instant::now();
     // let res2 = conv2d_naive(&a, &kernel, [1, 1])?;
     // println!("{:?}", now.elapsed());
-    assert!(res1.allclose(&res));
     Ok(())
 }
