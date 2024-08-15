c4adf4720f38764ab997d3b9cd95d63f215e7989
Jianqoq <ljj1849532909@gmail.com>
2024-08-14 22:32:48 -0400

    Refactor conv2d function for improved performance and readability

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use ops::cpu::convolutions::conv2d::{ conv2d, conv2d_naive, conv2d_block };
+use ops::cpu::convolutions::conv2d::{ conv2d, conv2d_block, conv2d_block_simd, conv2d_naive };
 use tensor_dyn::TensorCreator;
 use tensor_dyn::tensor_base::_Tensor;
 use tensor_dyn::*;
 
 fn main() -> anyhow::Result<()> {
     set_global_display_lr_elements(6);
-    let kernel = _Tensor::<i32>
+    let kernel = _Tensor::<f32>
         ::arange(0, 1024)?
         .reshape([8, 8, 4, 4])?
         .permute([2, 3, 0, 1])?
         .contiguous()?;
-    let a = _Tensor::<i32>
+    let a = _Tensor::<f32>
         ::arange(0, 8 * 256 * 128)?
         .reshape([8, 256, 128])?
         .permute([1, 2, 0])?
@@ -19,30 +19,30 @@ fn main() -> anyhow::Result<()> {
     let res1 = conv2d_block(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
     println!("{:?}", now.elapsed());
 
-    let kernel = _Tensor::<i32>
+    let kernel = _Tensor::<f32>
         ::arange(0, 1024)?
         .reshape([8, 8, 4, 4])?
         .permute([2, 3, 0, 1])?
         .contiguous()?;
-    let a = _Tensor::<i32>
+    let a = _Tensor::<f32>
         ::arange(0, 8 * 256 * 128)?
         .reshape([8, 256, 128])?
         .permute([1, 2, 0])?
         .contiguous()?;
     let now = std::time::Instant::now();
-    let _ = conv2d(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
+    let res = conv2d_block_simd(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
+    println!("{:?}", res);
     println!("{:?}", now.elapsed());
 
-    let kernel = _Tensor::<i32>
+    let kernel = _Tensor::<f32>
         ::arange(0, 1024)?
         .reshape([8, 8, 4, 4])?
         .permute([1, 0, 2, 3])?
         .contiguous()?;
-    let a = _Tensor::<i32>::arange(0, 8 * 256 * 128)?.reshape([8, 256, 128])?;
+    let a = _Tensor::<f32>::arange(0, 8 * 256 * 128)?.reshape([8, 256, 128])?;
     let now = std::time::Instant::now();
     let res2 = conv2d_naive(&a, &kernel, [1, 1])?;
     println!("{:?}", now.elapsed());
-
-    assert!(res1.allclose(&res2));
+    println!("{:?}", res2);
     Ok(())
 }
--- tensor-dyn/src/ops/cpu/convolutions/conv2d.rs
+++ tensor-dyn/src/ops/cpu/convolutions/conv2d.rs
@@ -6,10 +6,6 @@ use tensor_traits::TensorCreator;
 use tensor_types::type_promote::NormalOut;
 use crate::tensor_base::_Tensor;
 
-#[cfg(target_feature = "avx2")]
-use std::arch::x86_64;
-use std::arch::x86_64::__m256d;
-
 /// image: `[height, width, channels]`
 ///
 /// kernels: `[kernel_height, kernel_width, in_channels, out_channels]`
@@ -158,14 +154,20 @@ pub fn conv2d_block<T>(
     Ok(output)
 }
 
+use tensor_types::into_scalar::IntoScalar;
 #[cfg(target_feature = "fma")]
 pub fn conv2d_block_simd<T>(
     img: &_Tensor<T>,
     kernels: &_Tensor<T>,
     steps: [i64; 2]
-) -> anyhow::Result<_Tensor<T>>
-    where T: CommonBounds + std::ops::Mul<Output = T> + std::ops::AddAssign<T> + MulAdd<Output = T>
+)
+    -> anyhow::Result<_Tensor<T>>
+    where
+        T: CommonBounds + std::ops::Mul<Output = T> + std::ops::AddAssign<T> + MulAdd<Output = T>,
+        T: IntoScalar<f32>
 {
+    use wide::{ f32x4, f32x8 };
+
     let img_shape = img.shape();
     let img_height = img_shape[0];
     let img_width = img_shape[1];
@@ -203,7 +205,7 @@ pub fn conv2d_block_simd<T>(
     let ks2 = kernels.strides()[2]; // in_channels
     let ks3 = kernels.strides()[3]; // out_channels
 
-    let c_ob = 4;
+    let c_ob = 8;
     let c_ib = 4;
     let w_ob = 5;
     let jp_end = (out_channels + c_ob - 1) / c_ob;
@@ -219,11 +221,25 @@ pub fn conv2d_block_simd<T>(
                                 let i = ip * c_ib + i;
                                 for k in 0..w_ob {
                                     let k = kp * w_ob + k;
-                                    for j in 0..c_ob {
-                                        let j = jp * c_ob + j;
-                                        let k_val = kernel[i * ks2 + j * ks3 + m * ks1 + n * ks0];
-                                        let i_val = inp[i * is2 + (k * step_width + m) * is1 + (l * step_height + n) * is0]; // prettier-ignore
-                                        out[j * os2 + k * os1 + l * os0] += i_val * k_val;
+
+                                    let kernel_ptr = &kernel[i * ks2 + jp * c_ob * ks3 + m * ks1 + n * ks0] as *const T; // prettier-ignore
+                                    let kernel_vec = unsafe { std::slice::from_raw_parts(kernel_ptr, 8) }; // prettier-ignore
+                                    let kernel_vector = unsafe { f32x8::from(std::mem::transmute::<&[T], &[f32]>(kernel_vec)) }; // prettier-ignore
+
+                                    let res_ptr = &mut out[jp * c_ob * os2 + k * os1 + l * os0]; // prettier-ignore
+                                    let res_vec = unsafe { std::slice::from_raw_parts_mut(res_ptr, 8) }; // prettier-ignore
+                                    let res_vector = unsafe { f32x8::from(std::mem::transmute::<&[T], &[f32]>(res_vec)) }; // prettier-ignore
+
+                                    let i_val = inp[i * is2 + (k * step_width + m) * is1 + (l * step_height + n) * is0]; // prettier-ignore
+                                    let scalar_vec = f32x8::new([i_val.into_scalar(); 8]);
+                                    let res = kernel_vector.mul_add(scalar_vec, res_vector); // prettier-ignore
+
+                                    unsafe {
+                                        std::ptr::copy_nonoverlapping(
+                                            res.to_array().as_ptr() as *const f32,
+                                            res_vec.as_mut_ptr() as *mut f32,
+                                            8
+                                        );
                                     }
                                 }
                             }
