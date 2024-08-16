72dbd14a37035c53733bb6c1dfc2fc48fe4476e5
Jianqoq <ljj1849532909@gmail.com>
2024-08-16 02:36:41 -0400

    chore: Optimize code for conv2d function performance and readability

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,25 @@
-use ops::cpu::convolutions::conv2d::{
-    conv2d,
-    conv2d_block,
-    conv2d_block_parallel,
-    conv2d_block_simd,
-    conv2d_block_simd_parallel,
-    conv2d_naive,
-};
-use ops::cpu::convolutions::conv2d_unroll::{
-    conv2d_block_simd_parallel_unroll_f32,
-    conv2d_block_simd_parallel_unroll_i32,
-};
+use ops::cpu::convolutions::conv2d_unroll::conv2d_block_simd_parallel_unroll_f32_asm;
 use tensor_dyn::TensorCreator;
 use tensor_dyn::tensor_base::_Tensor;
 use tensor_dyn::*;
 
 fn main() -> anyhow::Result<()> {
     set_global_display_lr_elements(6);
-    set_num_threads(10);
-    let kernel = _Tensor::<i32>
-        ::arange(0, 10240)?
-        .reshape([8, 80, 4, 4])?
-        .permute([2, 3, 0, 1])?
-        .contiguous()?;
-    let a = _Tensor::<i32>
-        ::arange(0, 8 * 256 * 129)?
-        .reshape([8, 256, 129])?
-        .permute([1, 2, 0])?
-        .contiguous()?;
-    let now = std::time::Instant::now();
-    let res = conv2d(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
+    set_num_threads(1);
+    // let kernel = _Tensor::<i32>
+    //     ::arange(0, 10240)?
+    //     .reshape([8, 80, 4, 4])?
+    //     .permute([2, 3, 0, 1])?
+    //     .contiguous()?;
+    // let a = _Tensor::<i32>
+    //     ::arange(0, 8 * 256 * 129)?
+    //     .reshape([8, 256, 129])?
+    //     .permute([1, 2, 0])?
+    //     .contiguous()?;
+    // let now = std::time::Instant::now();
+    // let res = conv2d(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
 
-    println!("{:?}", now.elapsed());
+    // println!("{:?}", now.elapsed());
     let kernel = _Tensor::<f32>
         ::arange(0, 10240)?
         .reshape([8, 80, 4, 4])?
@@ -42,8 +31,10 @@ fn main() -> anyhow::Result<()> {
         .permute([1, 2, 0])?
         .contiguous()?;
     let now = std::time::Instant::now();
-    for i in 0..100 {
-        let _ = conv2d_block_simd_parallel_unroll_f32(&a, &kernel, [1, 1])?.permute([2, 0, 1])?;
+    for i in 0..1 {
+        let res = conv2d_block_simd_parallel_unroll_f32_asm(&a, &kernel, [1, 1])?.permute([
+            2, 0, 1,
+        ])?;
     }
     println!("{:?}", now.elapsed() / 100);
 
