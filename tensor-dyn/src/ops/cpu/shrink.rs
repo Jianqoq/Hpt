10a86e3fa0e6a4f80e4a2db9a95dd53e1eff3c41
Jianqoq <ljj1849532909@gmail.com>
2024-09-08 11:54:00 -0400

    Refactor shrink method for improved performance and readability

--- tensor-dyn/src/ops/cpu/shrink.rs
+++ tensor-dyn/src/ops/cpu/shrink.rs
@@ -3,13 +3,14 @@ use std::ops::{ Add, Neg, Sub };
 use tensor_traits::CommonBounds;
 use tensor_types::type_promote::NormalOut;
 use tensor_types::{ dtype::TypeCommon, type_promote::SimdCmp };
-use tensor_types::traits::Init;
+use tensor_types::traits::{ Init, SimdSelect };
 use crate::{ tensor::Tensor, tensor_base::_Tensor };
 
 impl<T> _Tensor<T>
     where
         T: CommonBounds + PartialOrd + Sub<Output = T> + Neg<Output = T> + Add<Output = T>,
-        <T as TypeCommon>::Vec: SimdCmp + NormalOut<Output = <T as TypeCommon>::Vec>
+        <T as TypeCommon>::Vec: SimdCmp + NormalOut<Output = <T as TypeCommon>::Vec>,
+        <<T as TypeCommon>::Vec as SimdCmp>::Output: SimdSelect<<T as TypeCommon>::Vec>
 {
     #[allow(unused)]
     pub fn shrink(&self, bias: T, lambda: T) -> anyhow::Result<_Tensor<T>> {
@@ -38,28 +39,32 @@ impl<T> _Tensor<T>
                             let sub_bias = y._sub(bias_vec);
                             let add_bias = y._add(bias_vec);
                             let zero = <T as TypeCommon>::Vec::splat(T::ZERO);
+                            let res = gt_mask.select(sub_bias, zero);
+                            *x = lt_mask.select(add_bias, res);
                         }
                     )
                     .collect()
             )
         }
         #[cfg(not(feature = "simd"))]
-        {}
-        // Ok(
-        //     self
-        //         .par_iter()
-        //         .strided_map(|x| {
-        //             if x > lambda { x - bias } else if x < -lambda { x + bias } else { T::ZERO }
-        //         })
-        //         .collect()
-        // )
+        {
+            Ok(
+                self
+                    .par_iter()
+                    .strided_map(|x| {
+                        if x > lambda { x - bias } else if x < -lambda { x + bias } else { T::ZERO }
+                    })
+                    .collect()
+            )
+        }
     }
 }
 
 impl<T> Tensor<T>
     where
         T: CommonBounds + PartialOrd + Sub<Output = T> + Neg<Output = T> + Add<Output = T>,
-        <T as TypeCommon>::Vec: SimdCmp + NormalOut<Output = <T as TypeCommon>::Vec>
+        <T as TypeCommon>::Vec: SimdCmp + NormalOut<Output = <T as TypeCommon>::Vec>,
+        <<T as TypeCommon>::Vec as SimdCmp>::Output: SimdSelect<<T as TypeCommon>::Vec>
 {
     pub fn shrink(&self, bias: T, lambda: T) -> anyhow::Result<Tensor<T>> {
         Ok(_Tensor::shrink(self, bias, lambda)?.into())
