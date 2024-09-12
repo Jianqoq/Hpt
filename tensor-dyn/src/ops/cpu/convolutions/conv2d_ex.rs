39be13ba04383209a8683f0c0f18d804300b90e9
Jianqoq <ljj1849532909@gmail.com>
2024-09-12 19:20:53 -0400

    Refactor conv2d_ex.rs for improved performance and readability

--- tensor-dyn/src/ops/cpu/convolutions/conv2d_ex.rs
+++ tensor-dyn/src/ops/cpu/convolutions/conv2d_ex.rs
@@ -473,8 +473,8 @@ impl<T> _Tensor<T>
                 (config.ci_block_size, config.co_block_size)
             }
         };
-        // let ci_b = 60;
-        // let co_b = 24;
+        let ci_b = 16;
+        let co_b = 32;
         let num_co_b = out_channels / co_b;
         let num_wo_b = out_width / (CONV_REGNUM as i64);
         let num_ci_b = in_channels / ci_b;
