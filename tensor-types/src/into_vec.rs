1feff9101c38e75d6417723ff6fa350fa6ebd550
JianJian Li <ljj1849532909@gail.com>
2024-09-15 02:04:21 -0400

    Refactor reduce_kernels.rs and into_vec.rs for improved performance and code readability

--- tensor-types/src/into_vec.rs
+++ tensor-types/src/into_vec.rs
@@ -15,6 +15,8 @@ mod into_vec {
 
 #[cfg(all(any(target_feature = "sse2", target_feature = "neon"), not(target_feature = "avx2")))]
 mod into_vec {
+    use tensor_macros::impl_into_vec;
+
     use super::IntoVec;
     use crate::vectors::_128bit::*;
     impl_into_vec!();
