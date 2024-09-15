d1b6a2ce5da3ece16b83b7ebcfad2fccbacf3f83
JianJian Li <ljj1849532909@gail.com>
2024-09-15 03:14:02 -0400

    Refactor tensor_dyn/reduce.rs for improved performance and code readability

--- tensor-tests/src/tensor_dyn/reduce.rs
+++ tensor-tests/src/tensor_dyn/reduce.rs
@@ -154,15 +154,15 @@ fn test_uncontiguous_sum2() -> anyhow::Result<()> {
 #[test]
 fn test_prod() -> anyhow::Result<()> {
     let (a, tch_a) = common_input(8 * 8096 * 2048, [8, 8096, 2048])?;
-    // let sum = a.prod(0, false)?;
-    // let tch_sum = tch_a.prod_dim_int(0, false, tch::Kind::Int64);
-    // assert_eq(&sum, &tch_sum);
-    // let sum = a.prod(1, false)?;
-    // let tch_sum = tch_a.prod_dim_int(1, false, tch::Kind::Int64);
-    // assert_eq(&sum, &tch_sum);
+    let sum = a.prod(0, false)?;
+    let tch_sum = tch_a.prod_dim_int(0, false, tch::Kind::Int64);
+    assert_eq(&sum, &tch_sum);
+    let sum = a.prod(1, false)?;
+    let tch_sum = tch_a.prod_dim_int(1, false, tch::Kind::Int64);
+    assert_eq(&sum, &tch_sum);
     let sum = a.prod(2, false)?;
-    // let tch_sum = tch_a.prod_dim_int(2, false, tch::Kind::Int64);
-    // assert_eq(&sum, &tch_sum);
+    let tch_sum = tch_a.prod_dim_int(2, false, tch::Kind::Int64);
+    assert_eq(&sum, &tch_sum);
 
     Ok(())
 }
