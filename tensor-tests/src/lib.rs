fba7887a96d56bbca06a772124ac70ab71fa94f1
123 <ljj1849532909@gmail.com>
2024-09-22 15:51:10 -0400

    a

--- tensor-tests/src/lib.rs
+++ tensor-tests/src/lib.rs
@@ -1,13 +1,13 @@
 pub mod tensor_dyn {
     pub mod conv2d;
-    // pub mod max_pool;
+    pub mod max_pool;
     pub mod reduce;
     // pub mod unary;
     pub mod creation;
     pub mod shape_manipulate;
     pub mod binary;
-    // pub mod avg_pool2d;
-    // pub mod lp_pool2d;
+    pub mod avg_pool2d;
+    pub mod lp_pool2d;
     // pub mod slice;
 }
 
