2e9430ec65f14d927069d11d20ccd87093eaf68c
123 <ljj1849532909@gmail.com>
2024-09-22 17:13:06 -0400

    a

--- tensor-tests/src/lib.rs
+++ tensor-tests/src/lib.rs
@@ -2,7 +2,7 @@ pub mod tensor_dyn {
     // pub mod conv2d;
     // pub mod max_pool;
     // pub mod reduce;
-    // pub mod unary;
+    pub mod unary;
     // pub mod creation;
     // pub mod shape_manipulate;
     pub mod binary;
