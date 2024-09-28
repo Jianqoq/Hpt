106cf2697e61015a94d51dee9282e4636edca3c9
Jianqoq <ljj1849532909@gmail.com>
2024-09-28 14:03:06 -0400

    a

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use ops::cpu::conv_config::{ Conv2dConfig, KernelParamAlgo };
 use tensor_dyn::tensor_base::_Tensor;
 use tensor_dyn::*;
 
-const IN: i64 = 512;
+const IN: i64 = 4096;
 
 fn main() -> anyhow::Result<()> {
-    set_num_threads(8);
+    set_num_threads(16);
     let kernel = _Tensor::<f32>
         ::arange(0, IN * 16 * 3 * 3)?
         .reshape([IN, 16, 3, 3])?
