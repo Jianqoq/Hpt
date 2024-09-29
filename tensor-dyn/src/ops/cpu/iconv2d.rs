df8ae673d02892b1269fcdd517b4fca6a1568239
Jianqoq <ljj1849532909@gmail.com>
2024-09-29 00:48:30 -0400

    t

--- tensor-dyn/src/ops/cpu/iconv2d.rs
+++ tensor-dyn/src/ops/cpu/iconv2d.rs
@@ -1,10 +1,10 @@
 use super::conv_config::Conv2dConfig;
 use crate::ops::cpu::conv_config::KernelParamAlgo;
 use crate::tensor_base::_Tensor;
-use crate::CONV_REGNUM;
 use rayon::prelude::*;
 use tensor_common::err_handler::ErrHandler;
 use tensor_common::err_handler::ErrHandler::InvalidInputShape;
+use tensor_common::err_handler::ErrHandler::InvalidCacheParam;
 use tensor_traits::CommonBounds;
 use tensor_traits::TensorCreator;
 use tensor_traits::TensorInfo;
@@ -47,6 +47,8 @@ impl<T> _Tensor<T>
         dilation: [i64; 2],
         config: Option<&Conv2dConfig<T>>
     ) -> anyhow::Result<_Tensor<T>> {
+        use crate::CONV_REGNUM;
+
         let img_shape = self.shape();
         if img_shape.len() != 4 {
             return Err(
@@ -61,10 +63,10 @@ impl<T> _Tensor<T>
         let img_width = img_shape[2];
         let img_channels = img_shape[3];
         let kernel_shape = kernels.shape();
-        let out_channels = kernel_shape[0];
-        let kernel_height = kernel_shape[1];
-        let kernel_width = kernel_shape[2];
-        let in_channels = kernel_shape[3];
+        let kernel_height = kernel_shape[0];
+        let kernel_width = kernel_shape[1];
+        let in_channels = kernel_shape[2];
+        let out_channels = kernel_shape[3];
         if in_channels != img_channels {
             panic!(
                 "The number of input channels in the image must be equal to the number of input channels in the kernel."
@@ -99,7 +101,7 @@ impl<T> _Tensor<T>
             };
         }
         let output = _Tensor::<T>::empty([batch, out_height, out_width, out_channels])?;
-        let mut out = output.ptr();
+        let out = output.ptr();
         let inp = img.ptr();
         let kernel = kernels.ptr();
 
@@ -111,55 +113,80 @@ impl<T> _Tensor<T>
         let ish = img.strides()[1]; // height
         let isw = img.strides()[2]; // width
 
-        let ks0 = kernels.strides()[0]; // kernel_out_channels
-        let ks1 = kernels.strides()[1]; // kernel_height
-        let ks2 = kernels.strides()[2]; // kernel_width
+        let ks0 = kernels.strides()[0]; // kernel_height
+        let ks1 = kernels.strides()[1]; // kernel_width
+        let ks2 = kernels.strides()[2]; // in_channels
 
-        let num_oh = out_height / 3;
+        let (ci_b, co_b) = match config {
+            Some(config) => (config.ci_block_size, config.co_block_size),
+            None => {
+                let config = {
+                    Conv2dConfig::<T>::new(
+                        out_channels,
+                        in_channels,
+                        [kernel_height, kernel_width],
+                        KernelParamAlgo::Greedy
+                    )
+                };
+                (config.ci_block_size, config.co_block_size)
+            }
+        };
+        if !(co_b % (T::Vec::SIZE as i64) == 0 || co_b == 1) || co_b > out_channels {
+            return Err(
+                InvalidCacheParam(
+                    "co_b",
+                    out_channels,
+                    T::Vec::SIZE as i64,
+                    co_b,
+                    core::panic::Location::caller()
+                ).into()
+            );
+        }
+
+        let num_oh = out_height / 4;
         let outer = batch * num_oh;
+
         (0..outer).into_par_iter().for_each(|idx| {
-            let mut out = output.ptr();
+            let mut out = out.clone();
             let b = idx / num_oh;
-            let lp = idx % num_oh;
-            for k in (0..out_width).step_by(3) {
-                for j in 0..out_channels {
-                    let mut results = [[T::ZERO; 4]; 3];
-                    for i in (0..in_channels).step_by(T::Vec::SIZE * 2) {
-                        for ll in 0..3 {
-                            let l = lp * 3 + ll as i64;
-                            let mut out_regs = [T::Vec::splat(T::ZERO); 4];
+            let ll = idx % num_oh;
+            for k in (0..out_width).step_by(4) {
+                for j in (0..out_channels).step_by(T::Vec::SIZE * 2) {
+                    for ii in (0..in_channels).step_by(T::Vec::SIZE * 2) {
+                        let ll = ll * 4;
+                        for l in 0..4 {
+                            let l = ll + l;
+                            let mut results = [[T::Vec::splat(T::ZERO); 4]; 2];
                             for n in 0..kernel_height {
-                                let inp_offset = b * isb + (l * step_height + n * dh) * ish;
                                 for m in 0..kernel_width {
-                                    let koffset = j * ks0 + n * ks1 + m * ks2;
-                                    unsafe {
-                                        let kernel_vec = T::Vec::from_ptr(&kernel[koffset + i]); // prettier-ignore
-                                        let inp0 = T::Vec::from_ptr(&inp[inp_offset + (k * step_width + m * dw) * isw + i]); // prettier-ignore
-                                        let inp1 = T::Vec::from_ptr(&inp[inp_offset + ((k + 1) * step_width + m * dw) * isw + i]); // prettier-ignore
-                                        let inp2 = T::Vec::from_ptr(&inp[inp_offset + ((k + 2) * step_width + m * dw) * isw + i]); // prettier-ignore
-                                        out_regs[0] = kernel_vec.mul_add(inp0, out_regs[0]);
-                                        out_regs[1] = kernel_vec.mul_add(inp1, out_regs[1]);
-                                        out_regs[2] = kernel_vec.mul_add(inp2, out_regs[2]);
-                                        let kernel_vec = T::Vec::from_ptr(&kernel[koffset + i + T::Vec::SIZE as i64]); // prettier-ignore
-                                        let inp0 = T::Vec::from_ptr(&inp[inp_offset + (k * step_width + m * dw) * isw + i + T::Vec::SIZE as i64]); // prettier-ignore
-                                        let inp1 = T::Vec::from_ptr(&inp[inp_offset + ((k + 1) * step_width + m * dw) * isw + i + T::Vec::SIZE as i64]); // prettier-ignore
-                                        let inp2 = T::Vec::from_ptr(&inp[inp_offset + ((k + 2) * step_width + m * dw) * isw + i + T::Vec::SIZE as i64]); // prettier-ignore
-                                        out_regs[0] = kernel_vec.mul_add(inp0, out_regs[0]);
-                                        out_regs[1] = kernel_vec.mul_add(inp1, out_regs[1]);
-                                        out_regs[2] = kernel_vec.mul_add(inp2, out_regs[2]);
+                                    for i in 0..(T::Vec::SIZE as i64) * 2 {
+                                        let i = ii + i;
+                                        unsafe {
+                                            let inp0 = T::Vec::splat(inp[b * isb + (l * step_height + n) * ish + (k * step_width + m) * isw + i]); // prettier-ignore
+                                            let inp1 = T::Vec::splat(inp[b * isb + (l * step_height + n) * ish + ((k + 1) * step_width + m) * isw + i]); // prettier-ignore
+                                            let inp2 = T::Vec::splat(inp[b * isb + (l * step_height + n) * ish + ((k + 2) * step_width + m) * isw + i]); // prettier-ignore
+                                            let inp3 = T::Vec::splat(inp[b * isb + (l * step_height + n) * ish + ((k + 3) * step_width + m) * isw + i]); // prettier-ignore
+
+                                            for v in 0..2 {
+                                                let kernel = T::Vec::from_ptr(&kernel[n * ks0 + m * ks1 + i * ks2 + j + v * T::Vec::SIZE as i64]); // prettier-ignore
+                                                results[v as usize][0] = inp0.mul_add(kernel, results[v as usize][0]); // prettier-ignore
+                                                results[v as usize][1] = inp1.mul_add(kernel, results[v as usize][1]); // prettier-ignore
+                                                results[v as usize][2] = inp2.mul_add(kernel, results[v as usize][2]); // prettier-ignore
+                                                results[v as usize][3] = inp3.mul_add(kernel, results[v as usize][3]); // prettier-ignore
+                                            }
+                                        }
                                     }
                                 }
                             }
-                            results[ll][0] = out_regs[0].sum()._add(results[ll][0]);
-                            results[ll][1] = out_regs[1].sum()._add(results[ll][1]);
-                            results[ll][2] = out_regs[2].sum()._add(results[ll][2]);
+                            for v in 0..2 {
+                                for kk in 0..4 {
+                                    let out_vec = &mut out[b * osb + l * osh + (k + kk) * osw + j + v * T::Vec::SIZE as i64] as *mut _ as *mut T::Vec; // prettier-ignore
+                                    unsafe {
+                                        out_vec.write_unaligned(results[v as usize][kk as usize]);
+                                    }
+                                }
+                            }
                         }
-                    }
-                    for ll in 0..3 {
-                        let l = lp * 3 + ll as i64;
-                        out[b * osb + l * osh + k * osw + j] = results[ll][0];
-                        out[b * osb + l * osh + (k + 1) * osw + j] = results[ll][1];
-                        out[b * osb + l * osh + (k + 2) * osw + j] = results[ll][2];
                     }
                 }
             }
