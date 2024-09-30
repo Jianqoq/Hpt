7520dd5147cd8994f23f6974c7ffcbaa82d1b33d
Jianqoq <ljj1849532909@gmail.com>
2024-09-30 01:50:31 -0400

    a

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use ops::cpu::conv_config::{ Conv2dConfig, KernelParamAlgo };
 use tensor_dyn::tensor_base::_Tensor;
 use tensor_dyn::*;
 
-const IN: i64 = 128;
-const OUT: i64 = 4096 * 2;
+const IN: i64 = 8192;
+const OUT: i64 = 128;
 const KH: i64 = 3;
 const KW: i64 = 3;
 const H: i64 = 224;
