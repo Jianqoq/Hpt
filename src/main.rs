f2a029f9ed735be07e6b16c60ef1fd49f4193a83
Jianqoq <ljj1849532909@gmail.com>
2024-10-20 01:20:53 -0400

    a

--- src/main.rs
+++ src/main.rs
@@ -4,68 +4,19 @@ use tensor_dyn::tensor_base::_Tensor;
 use tensor_dyn::*;
 
 fn main() -> anyhow::Result<()> {
-    set_num_threads(16);
-    let ic = 16;
-    let kh = 3;
-    let kw = 3;
-    let h = 256;
-    let w = 256;
-    let kernel = _Tensor::<f32>
-        ::arange(0, ic * kh * kw)?
-        .reshape(&[ic, kh, kw])?
-        // .permute([0, 2, 3, 1])?
-        .permute([1, 2, 0])?
-        .contiguous()?;
-    // let kernel = _Tensor::<f32>::randn([kh, kw, ic, oc])?;
-    let a = _Tensor::<f32>
-        ::arange(0, 1 * ic * h * w)?
-        .reshape(&[1, ic, h, w])?
-        .permute([0, 2, 3, 1])?
-        .contiguous()?;
-    // let a = _Tensor::<f32>::randn([1, h, w, ic])?;
-    let now = std::time::Instant::now();
-    for _ in 0..1 {
-        let res = a.conv2d_group(
-            &kernel,
-            None,
-            [1, 1],
-            [
-                (0, 0),
-                (0, 0),
-            ],
-            [1, 1],
-            2,
-            None
-        )?;
-        println!("{:?}", res);
-        // let res2 = a.conv2d(
-        //     &kernel,
-        //     [1, 1],
-        //     [
-        //         (0, 0),
-        //         (0, 0),
-        //     ],
-        //     [1, 1],
-        //     Some(&config)
-        // )?;
-        // assert_eq!(res, res2);
-    }
-    println!("{:?}", now.elapsed() / 1);
-    // conv2d()?;
-    // println!("{}", 1u32 & 31u32);
-
+    conv2d()?;
     Ok(())
 }
 
 fn conv2d() -> Result<(), anyhow::Error> {
-    let oc_sets = [128, 256, 512, 1024, 2048];
-    let ic_sets = [128, 256, 512, 1024, 2048];
+    let oc_sets = [2048];
+    let ic_sets = [2048];
     let kh_sets = [3];
     let kw_sets = [3];
     let h_sets = [256];
     let w_sets = [256];
 
-    set_num_threads(16);
+    set_num_threads(1);
     let mut workbook = Workbook::new();
     let decimal_format = Format::new().set_num_format("0.0000000000");
     let format = Format::new();
@@ -79,10 +30,9 @@ fn conv2d() -> Result<(), anyhow::Error> {
                     for h in h_sets {
                         for w in w_sets {
                             let kernel = _Tensor::<f32>
-                                ::arange(0, ic * kh * kw)?
-                                .reshape([ic, kh, kw])?
-                                // .permute([0, 2, 3, 1])?
-                                .permute([1, 2, 0])?
+                                ::arange(0, 1 * ic * kh * kw)?
+                                .reshape([ic, 1, kh, kw])?
+                                .permute([2, 3, 1, 0])?
                                 .contiguous()?;
                             let a = _Tensor::<f32>
                                 ::arange(0, 1 * ic * h * w)?
@@ -93,7 +43,7 @@ fn conv2d() -> Result<(), anyhow::Error> {
                             // let a = Tensor::randn(1.0, 1.0, &[1, ic, h, w], &device)?;
                             // let kernel = Tensor::randn(1.0, 1.0, &[oc, ic, kh, kw], &device)?;
                             let now = std::time::Instant::now();
-                            let _ = a.conv2d_group(
+                            let res = a.conv2d_group(
                                 &kernel,
                                 None,
                                 [1, 1],
@@ -102,9 +52,10 @@ fn conv2d() -> Result<(), anyhow::Error> {
                                     (0, 0),
                                 ],
                                 [1, 1],
-                                2,
+                                ic,
                                 None
                             )?;
+                            // println!("{:?}", res.shape());
                             worksheet.write_number(
                                 row,
                                 0,
