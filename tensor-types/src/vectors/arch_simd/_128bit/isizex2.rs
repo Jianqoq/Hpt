886c4c67ffe0a6e02d75d0c23bd10b6a048ef75c
Jianqoq <ljj1849532909@gmail.com>
2024-12-13 18:00:59 -0500

    a

--- tensor-types/src/vectors/arch_simd/_128bit/isizex2.rs
+++ tensor-types/src/vectors/arch_simd/_128bit/isizex2.rs
@@ -346,7 +346,7 @@ impl std::ops::Neg for isizex2 {
     fn neg(self) -> Self {
         #[cfg(target_pointer_width = "64")]
         {
-            isizex2(unsafe { _mm_sign_epi64(self.0, _mm_set1_epi64x(-1)) })
+            unsafe { isizex2(_mm_sub_epi64(_mm_setzero_si128(), self.0)) }
         }
         #[cfg(target_pointer_width = "32")]
         {
