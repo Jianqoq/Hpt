8094ba7a0110c2ad436cefdaf02a3bd57d95bd7b
Jianqoq <ljj1849532909@gmail.com>
2024-12-31 11:38:10 -0500

    a

--- tensor-types/src/scalars/_f32.rs
+++ tensor-types/src/scalars/_f32.rs
@@ -26,7 +26,12 @@ impl NormalOut2 for f32 {
 
     #[inline(always)]
     fn __mul_add(self, a: Self, b: Self) -> Self {
-        (self * a) + b
+        #[cfg(target_feature = "fma")]
+        unsafe {
+            std::intrinsics::fmaf32(self, a, b)
+        }
+        #[cfg(not(target_feature = "fma"))]
+        return std::hint::black_box((self * a) + b);
     }
 
     #[inline(always)]
