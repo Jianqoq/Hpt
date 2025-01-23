047b117213ae1921e77b8484b96dd03a70bff926
Jianqoq <ljj1849532909@gmail.com>
2025-01-23 04:47:03 -0500

    a

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,13 @@ import { defineUserConfig } from '@vuepress/cli'
 import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
+  base: process.env.NODE_ENV === 'development'
+    ? '/'
+    : '/eTensor/',
   lang: 'zh-CN',
   title: 'HPT 文档',
   description: '这是项目文档',
-  
+
   theme: defaultTheme({
     navbar: [
       {
