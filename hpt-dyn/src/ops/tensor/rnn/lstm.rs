ecb79b19860b7366b5aed5dd2828089e4a4635b8
JianJian Li <ljj1849532909@gmail.com>
2025-05-18 20:51:43 -0400

    Refactor LSTM implementation for improved readability and consistency

--- hpt-dyn/src/ops/tensor/rnn/lstm.rs
+++ hpt-dyn/src/ops/tensor/rnn/lstm.rs
@@ -1,20 +1,20 @@
 use std::sync::Arc;
 
 use crate::{
-    ops::tensor::matmul::{ matmul::addmm_prepacked_, microkernel_trait::MatmulMicroKernel },
     Tensor,
+    ops::tensor::matmul::{matmul::addmm_prepacked_, microkernel_trait::MatmulMicroKernel},
 };
 use hpt_common::{
-    error::base::TensorError,
-    layout::layout::Layout,
-    shape::shape_utils::mt_intervals,
-    Pointer,
+    Pointer, error::base::TensorError, layout::layout::Layout, shape::shape_utils::mt_intervals,
 };
 use hpt_macros::select;
 use hpt_matmul::Zero;
-use hpt_traits::tensor::{ CommonBounds, TensorInfo };
-use hpt_types::{ dtype::{ DType, ToDType }, type_promote::FloatOutUnary };
-use rayon::iter::{ IntoParallelIterator, ParallelIterator };
+use hpt_traits::tensor::{CommonBounds, TensorInfo};
+use hpt_types::{
+    dtype::{DType, ToDType},
+    type_promote::FloatOutUnary,
+};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Direction {
@@ -55,9 +55,10 @@ fn lstm_post_process<T: CommonBounds>(
     gates_b_stride: i64,
     hidden_size: i64,
     activate1: impl (Fn(T) -> T) + Send + Sync + Copy,
-    activate2: impl (Fn(T) -> T) + Send + Sync + Copy
-)
-    where T: FloatOutUnary<Output = T>, T::Vec: FloatOutUnary<Output = T::Vec>
+    activate2: impl (Fn(T) -> T) + Send + Sync + Copy,
+) where
+    T: FloatOutUnary<Output = T>,
+    T::Vec: FloatOutUnary<Output = T::Vec>,
 {
     let mut y = y + batch_step * y_b_stride + time_step * y_sq_stride;
 
@@ -114,23 +115,22 @@ fn lstm_post_process<T: CommonBounds>(
 }
 
 fn lstm_step<T: CommonBounds>(
-    x: &Tensor, // [seq_len, batch_size, input_size]
-    w_t: &Tensor, // [input_size, 4 * hidden_size]
-    r_t: &Tensor, // [hidden_size, 4 * hidden_size]
-    b: Option<&Tensor>, // [8 * hidden_size]
-    p: Option<&Tensor>, // [3*hidden_size]
-    initial_h: &Tensor, // [batch_size, hidden_size]
-    initial_c: &Tensor, // [batch_size, hidden_size],
+    x: &Tensor,                // [seq_len, batch_size, input_size]
+    w_t: &Tensor,              // [input_size, 4 * hidden_size]
+    r_t: &Tensor,              // [hidden_size, 4 * hidden_size]
+    b: Option<&Tensor>,        // [8 * hidden_size]
+    p: Option<&Tensor>,        // [3*hidden_size]
+    initial_h: &Tensor,        // [batch_size, hidden_size]
+    initial_c: &Tensor,        // [batch_size, hidden_size],
     seq_lens: Option<&Tensor>, // [batch_size]
     direction: Direction,
     hidden_size: i64,
     activate1: impl (Fn(T) -> T) + Send + Sync + Copy,
-    activate2: impl (Fn(T) -> T) + Send + Sync + Copy
-)
-    -> Result<(Tensor, Tensor, Tensor), TensorError>
-    where
-        T: FloatOutUnary<Output = T> + hpt_matmul::MatmulMicroKernel + Zero,
-        T::Vec: FloatOutUnary<Output = T::Vec>
+    activate2: impl (Fn(T) -> T) + Send + Sync + Copy,
+) -> Result<(Tensor, Tensor, Tensor), TensorError>
+where
+    T: FloatOutUnary<Output = T> + hpt_matmul::MatmulMicroKernel + Zero,
+    T::Vec: FloatOutUnary<Output = T::Vec>,
 {
     let seq_len = x.shape()[0];
     let batch_size = x.shape()[1];
@@ -154,19 +154,22 @@ fn lstm_step<T: CommonBounds>(
     let h_t = initial_h.clone(); // [batch_size, hidden_size]
     let c_t = initial_c.clone(); // [batch_size, hidden_size]
 
-    let y = Tensor::empty(&[seq_len, batch_size, hidden_size], x.dtype, x.device.clone())?;
+    let y = Tensor::empty(
+        &[seq_len, batch_size, hidden_size],
+        x.dtype,
+        x.device.clone(),
+    )?;
     let mut final_h = Tensor::empty(&[batch_size, hidden_size], x.dtype, x.device.clone())?;
     let mut final_c = Tensor::empty(&[batch_size, hidden_size], x.dtype, x.device.clone())?;
 
     let x_reshaped = x.reshape(&[seq_len * batch_size, -1])?;
 
-    let tmp = (
-        if let Some(sum_bias) = &sum_bias {
-            x_reshaped.addmm(w_t, sum_bias)?
-        } else {
-            x_reshaped.matmul(w_t)?
-        }
-    ).reshape(&[seq_len, batch_size, 4 * hidden_size])?; // [seq_len, batch_size, 4 * hidden_size]
+    let tmp = (if let Some(sum_bias) = &sum_bias {
+        x_reshaped.addmm(w_t, sum_bias)?
+    } else {
+        x_reshaped.matmul(w_t)?
+    })
+    .reshape(&[seq_len, batch_size, 4 * hidden_size])?; // [seq_len, batch_size, 4 * hidden_size]
 
     let batch_size = x.shape()[1];
 
@@ -176,18 +179,13 @@ fn lstm_step<T: CommonBounds>(
     let batch_parallel = (batch_size as usize) > crate::physical_cores();
 
     if batch_parallel {
-        let func = |
-            start: i64,
-            end: i64,
-            y: &Tensor,
-            prepacked_r: Arc<Vec<hpt_matmul::NewPrePackedRhs>>
-        | -> Result<(), TensorError> {
+        let func = |start: i64,
+                    end: i64,
+                    y: &Tensor,
+                    prepacked_r: Arc<Vec<hpt_matmul::NewPrePackedRhs>>|
+         -> Result<(), TensorError> {
             let global_now = std::time::Instant::now();
-            let gates = Tensor::empty(
-                &[end - start, 4 * hidden_size],
-                x.dtype,
-                x.device.clone()
-            )?;
+            let gates = Tensor::empty(&[end - start, 4 * hidden_size], x.dtype, x.device.clone())?;
             let h_t = initial_h.slice(&select![start:end, ..])?;
             let c_t = initial_c.slice(&select![start:end, ..])?;
             let tmp = tmp.slice(&select![:, start:end, ..])?;
@@ -197,10 +195,10 @@ fn lstm_step<T: CommonBounds>(
             let r_t_layout = r_t.layout();
             let gates_ptr = gates.ptr::<u8>();
             let gates_layout = gates.layout();
-            let bias_layout = Layout::new([tmp.shape()[1]], [tmp.strides()[1]]);
+            let bias_layout = Layout::new([tmp.shape()[1], tmp.shape()[2]], [tmp.strides()[1], tmp.strides()[2]]);
             let y_layout = Layout::new(
                 [y.shape()[1], y.shape()[2]],
-                [y.strides()[1], y.strides()[2]]
+                [y.strides()[1], y.strides()[2]],
             );
 
             let mut total_mm = std::time::Duration::from_secs(0);
@@ -219,10 +217,9 @@ fn lstm_step<T: CommonBounds>(
                     gates_layout,
                     1,
                     x.dtype,
-                    Some(prepacked_r.clone())
+                    Some(prepacked_r.clone()),
                 )?;
                 total_mm += now.elapsed();
-
                 let now = std::time::Instant::now();
                 for b in 0..end - start {
                     lstm_post_process(
@@ -238,7 +235,7 @@ fn lstm_step<T: CommonBounds>(
                         gates.strides()[0],
                         hidden_size,
                         activate1,
-                        activate2
+                        activate2,
                     );
                 }
                 total_post += now.elapsed();
@@ -272,9 +269,9 @@ fn lstm_step<T: CommonBounds>(
             Ok(())
         };
         let chunks = mt_intervals(batch_size as usize, crate::physical_cores());
-        let same_chunk = chunks
-            .iter()
-            .fold(true, |acc, (start, end)| { acc && end - start == chunks[0].1 - chunks[0].0 });
+        let same_chunk = chunks.iter().fold(true, |acc, (start, end)| {
+            acc && end - start == chunks[0].1 - chunks[0].0
+        });
         let prepacked_r = if same_chunk {
             let prepacked_r = hpt_matmul::prepack_rhs(
                 4 * (hidden_size as usize),
@@ -282,15 +279,17 @@ fn lstm_step<T: CommonBounds>(
                 hidden_size as usize,
                 r_t.ptr::<T>().ptr as *const T,
                 [r_t.strides()[0], r_t.strides()[1]],
-                1
+                1,
             );
             Arc::new(prepacked_r)
         } else {
             panic!("LSTM step: different chunk size is not supported yet");
         };
         let now = std::time::Instant::now();
-        chunks.into_par_iter().for_each(|(start, end)| {
-            let y = y.slice(&select![:, start as i64:end as i64, ..]).expect("lstm step error");
+        chunks.into_iter().for_each(|(start, end)| {
+            let y = y
+                .slice(&select![:, start as i64:end as i64, ..])
+                .expect("lstm step error");
             func(start as i64, end as i64, &y, prepacked_r.clone()).expect("lstm step error");
         });
         println!("time taken: {:?}", now.elapsed());
@@ -303,7 +302,7 @@ fn lstm_step<T: CommonBounds>(
             hidden_size as usize,
             r_t.ptr::<T>().ptr as *const T,
             [r_t.strides()[0], r_t.strides()[1]],
-            num_threads
+            num_threads,
         );
         let mut h_t_ptr = h_t.ptr::<u8>();
         let mut h_t_layout = h_t.layout();
@@ -313,7 +312,7 @@ fn lstm_step<T: CommonBounds>(
         let gates_layout = gates.layout();
         let y_layout = Layout::new(
             [y.shape()[1], y.shape()[2]],
-            [y.strides()[1], y.strides()[2]]
+            [y.strides()[1], y.strides()[2]],
         );
 
         let prepacked_r = Arc::new(prepacked_r);
@@ -331,7 +330,7 @@ fn lstm_step<T: CommonBounds>(
                 gates_layout,
                 1,
                 x.dtype,
-                Some(prepacked_r.clone())
+                Some(prepacked_r.clone()),
             )?;
             let gates_b_stride = gates.strides()[0] as i64;
             for b in 0..batch_size {
@@ -348,7 +347,7 @@ fn lstm_step<T: CommonBounds>(
                     gates_b_stride,
                     hidden_size,
                     activate1,
-                    activate2
+                    activate2,
                 );
             }
 
@@ -377,27 +376,26 @@ fn lstm_step<T: CommonBounds>(
 }
 
 fn lstm_cell_ref<T>(
-    x: &Tensor, // [seq_length, batch_size, input_size]
-    w: &Tensor, // [num_directions, 4 * hidden_size, input_size]
-    r: &Tensor, // [num_directions, 4 * hidden_size, hidden_size]
-    b: Option<&Tensor>, // [num_directions, 8 * hidden_size]
+    x: &Tensor,                // [seq_length, batch_size, input_size]
+    w: &Tensor,                // [num_directions, 4 * hidden_size, input_size]
+    r: &Tensor,                // [num_directions, 4 * hidden_size, hidden_size]
+    b: Option<&Tensor>,        // [num_directions, 8 * hidden_size]
     seq_lens: Option<&Tensor>, // [batch_size]
-    init_h: Option<&Tensor>, // [num_directions, batch_size, hidden_size]
-    init_c: Option<&Tensor>, // [num_directions, batch_size, hidden_size]
-    p: Option<&Tensor>, // [num_directions, 3*hidden_size],
+    init_h: Option<&Tensor>,   // [num_directions, batch_size, hidden_size]
+    init_c: Option<&Tensor>,   // [num_directions, batch_size, hidden_size]
+    p: Option<&Tensor>,        // [num_directions, 3*hidden_size],
     direction: Direction,
     f: impl (Fn(T) -> T) + Send + Sync + Copy,
-    g: impl (Fn(T) -> T) + Send + Sync + Copy
-)
-    -> Result<(Tensor, Tensor, Tensor), TensorError>
-    where
-        T: FloatOutUnary<Output = T> +
-            MatmulMicroKernel +
-            ToDType +
-            CommonBounds +
-            hpt_matmul::MatmulMicroKernel +
-            Zero,
-        T::Vec: FloatOutUnary<Output = T::Vec>
+    g: impl (Fn(T) -> T) + Send + Sync + Copy,
+) -> Result<(Tensor, Tensor, Tensor), TensorError>
+where
+    T: FloatOutUnary<Output = T>
+        + MatmulMicroKernel
+        + ToDType
+        + CommonBounds
+        + hpt_matmul::MatmulMicroKernel
+        + Zero,
+    T::Vec: FloatOutUnary<Output = T::Vec>,
 {
     let seq_length = x.shape()[0];
     let batch_size = x.shape()[1];
@@ -410,18 +408,26 @@ fn lstm_cell_ref<T>(
     let y = Tensor::empty(
         &[seq_length, num_directions, batch_size, hidden_size],
         x.dtype,
-        x.device.clone()
+        x.device.clone(),
     )?;
     let y_h = if let Some(h) = init_h {
         h.clone()
     } else {
-        Tensor::zeros(&[num_directions, batch_size, hidden_size], x.dtype, x.device.clone())?
+        Tensor::zeros(
+            &[num_directions, batch_size, hidden_size],
+            x.dtype,
+            x.device.clone(),
+        )?
     };
 
     let y_c = if let Some(c) = init_c {
         c.clone()
     } else {
-        Tensor::zeros(&[num_directions, batch_size, hidden_size], x.dtype, x.device.clone())?
+        Tensor::zeros(
+            &[num_directions, batch_size, hidden_size],
+            x.dtype,
+            x.device.clone(),
+        )?
     };
 
     for dir in 0..num_directions {
@@ -429,7 +435,11 @@ fn lstm_cell_ref<T>(
             Direction::Forward => Direction::Forward,
             Direction::Reverse => Direction::Reverse,
             Direction::Bidirectional => {
-                if dir == 0 { Direction::Forward } else { Direction::Reverse }
+                if dir == 0 {
+                    Direction::Forward
+                } else {
+                    Direction::Reverse
+                }
             }
         };
 
@@ -460,7 +470,7 @@ fn lstm_cell_ref<T>(
             direction,
             hidden_size,
             f,
-            g
+            g,
         )?;
 
         let mut y_local = y.slice(&select![:, dir:dir + 1, ..])?.squeeze(&[1])?;
@@ -483,63 +493,55 @@ impl Tensor {
         init_h: Option<&Tensor>,
         init_c: Option<&Tensor>,
         p: Option<&Tensor>,
-        direction: &str
+        direction: &str,
     ) -> Result<(Tensor, Tensor, Tensor), TensorError> {
         let direction = Direction::from_str(direction);
         let (y, y_h, y_c) = match self.dtype {
             DType::F32 => {
                 #[inline(always)]
                 fn sigmoid(x: f32) -> f32 {
-                    if x > 0.0 { 1.0 / (1.0 + (-x).exp()) } else { x.exp() / (1.0 + x.exp()) }
+                    if x > 0.0 {
+                        1.0 / (1.0 + (-x).exp())
+                    } else {
+                        x.exp() / (1.0 + x.exp())
+                    }
                 }
                 #[inline(always)]
                 fn tanh(x: f32) -> f32 {
                     2.0 * sigmoid(2.0 * x) - 1.0
                 }
                 lstm_cell_ref::<f32>(
-                    self,
-                    w,
-                    r,
-                    b,
-                    seq_lens,
-                    init_h,
-                    init_c,
-                    p,
-                    direction,
-                    sigmoid,
-                    tanh
+                    self, w, r, b, seq_lens, init_h, init_c, p, direction, sigmoid, tanh,
                 )?
             }
             #[cfg(feature = "f16")]
-            DType::F16 =>
-                lstm_cell_ref::<half::f16>(
-                    self,
-                    w,
-                    r,
-                    b,
-                    seq_lens,
-                    init_h,
-                    init_c,
-                    p,
-                    direction,
-                    |x| x._sigmoid(),
-                    |x| x._tanh()
-                )?,
+            DType::F16 => lstm_cell_ref::<half::f16>(
+                self,
+                w,
+                r,
+                b,
+                seq_lens,
+                init_h,
+                init_c,
+                p,
+                direction,
+                |x| x._sigmoid(),
+                |x| x._tanh(),
+            )?,
             #[cfg(feature = "bf16")]
-            DType::BF16 =>
-                lstm_cell_ref::<half::bf16>(
-                    self,
-                    w,
-                    r,
-                    b,
-                    seq_lens,
-                    init_h,
-                    init_c,
-                    p,
-                    direction,
-                    |x| x._sigmoid(),
-                    |x| x._tanh()
-                )?,
+            DType::BF16 => lstm_cell_ref::<half::bf16>(
+                self,
+                w,
+                r,
+                b,
+                seq_lens,
+                init_h,
+                init_c,
+                p,
+                direction,
+                |x| x._sigmoid(),
+                |x| x._tanh(),
+            )?,
             _ => unimplemented!(),
         };
         Ok((y, y_h, y_c))
